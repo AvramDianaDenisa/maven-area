9b930854e1a022954f511c4fddf45078f21feda6
AvramDianaDenisa <139481014+AvramDianaDenisa@users.noreply.github.com>
2023-07-14 14:18:52 +0300

    Add files via upload

---
+++ firstmaven/src/main/java/org/avramdiana/proiect/Main.java
@@ -0,0 +1,14 @@
+package org.avramdiana.proiect;
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+        double sideLength = 5.0;
+        double area = sideLength * sideLength;
+
+        System.out.println("Aria pătratului cu latura " + sideLength + " este: " + area);
+    }
+}
